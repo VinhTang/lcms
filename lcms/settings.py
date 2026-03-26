296f66aa4a47f0d8fc1e67be605e92ede6223c44
VinhTT4 <vinhtt4@vng.com.vn>
2026-03-26 22:47:21 +0700

    lcms

--- lcms/settings.py
+++ lcms/settings.py
@@ -129,7 +129,7 @@ USE_I18N = True
 USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
-STATICFILES_DIRS = [BASE_DIR / "static"]
+# STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
